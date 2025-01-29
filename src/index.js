c170d7fc4776aafea9bf323fcdd57a2b8db1e5a7
mrfle <mrfle4er@gmail.com>
2025-01-29 14:24:36 +0500

    step 6

--- src/index.js
+++ src/index.js
@@ -2,36 +2,16 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import {createRoot} from "react-dom/client";
 
-//const element = React.createElement('hi', null, 'Hello React World');
-const ToDoList = () => {
-    const items = ['Learn React', 'Study Machine Learning', 'Get job in AirAstana', 'Have fun!']
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-            <li>{items[2]}</li>
-            <li>{items[3]}</li>
-        </ul>
-    )
-}
-const AppHeader = () => {
-    return <h1>My Todo List</h1>
-}
-const SearchPanel = () => {
-    const searchText = 'Type here to search...';
-    const searchStyle = {
-        fontSize: '14px',
-    };
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-    return <input style={searchStyle} placeholder={searchText} disabled={true} />;
-}
 const App = () => {
-    //const value = '<script>alert ("")</script>';
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <ToDoList />
+            <TodoList />
         </div>
     )
 }
