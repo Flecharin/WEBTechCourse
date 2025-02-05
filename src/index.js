75a03e20f9a5a3a022343300b87123451f46400c
mrfle <mrfle4er@gmail.com>
2025-02-05 12:27:10 +0500

    step 17 Project directories

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createRoot} from "react-dom/client";
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemList from "./components/item-status-filter";
-import ItemStatusFilter from "./components/item-status-filter";
-
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Implement React App', important: true, id: 2},
-        {label: 'Make notes from ur stud', important: false, id: 3},
-        {label: 'Do not drink alcohol', important: false, id:4},
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3}/>
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
+import App from './components/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
