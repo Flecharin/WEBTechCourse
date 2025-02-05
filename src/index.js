5b29aa1098539f6e9a8e5fa942fa426ee2f09402
mrfle <mrfle4er@gmail.com>
2025-02-05 09:48:08 +0500

    step 13 Todo List Btn status filter

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import {createRoot} from "react-dom/client";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemList from "./components/item-status-filter";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
     const todoData = [
@@ -18,6 +20,7 @@ const App = () => {
             <AppHeader />
             <SearchPanel />
             <TodoList todos = {todoData}/>
+            <ItemStatusFilter />
         </div>
     )
 }
